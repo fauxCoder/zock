da0cddcb482561cd637d6b957b0a0cb2f95ba8d5
fauxCoder <jamesonyork8@gmail.com>
2024-04-07 23:00:45 +1000

    Updates name.

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "basic",
+        .name = "zock",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
